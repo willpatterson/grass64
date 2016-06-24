51509323e9aebf062eb56fe577391467f01a9187
Will Patterson <wpatt2@pdx.edu>
2016-06-24 15:57:29 -0700

    Initial commit with roll files

---
+++ src/grass64/version.mk
@@ -0,0 +1,5 @@
+PKGROOT		= /opt/grass64
+NAME        = test
+VERSION     = 1.0
+RELEASE 	= 1
+TARBALL_POSTFIX	= tgz
---
+++ version.mk
@@ -0,0 +1,6 @@
+ROLLNAME	= grass64
+VERSION		= 1.1
+RELEASE		= 4
+COLOR		= mediumslateblue
+
+REDHAT.ROOT	= $(CURDIR)
